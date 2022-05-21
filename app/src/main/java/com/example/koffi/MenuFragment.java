19ffa1e0d90ba82fb2061da6aba3ba61f1302c1e
Trang <thutrangpham1509@gmail.com>
2022-05-21 20:15:35 +0700

    fix bug menu display

--- app/src/main/java/com/example/koffi/MenuFragment.java
+++ app/src/main/java/com/example/koffi/MenuFragment.java
@@ -28,6 +28,9 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldPath;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -77,7 +80,8 @@ public class MenuFragment extends Fragment {
         recyclerView = view.findViewById(R.id.menu_recycler);
 
         menuArray = new ArrayList<Category>();
-        getMenuArray();
+//        getMenuArray();
+        loadMenu();
 
         categoryAdapter = new CategoryAdapter(getContext(),menuArray);
         gridView.setAdapter(categoryAdapter);
@@ -90,41 +94,93 @@ public class MenuFragment extends Fragment {
 
     }
 
-
-    public void getMenuArray() {
-        db.collection("menu").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+    private void loadMenu() {
+        new Thread(new Runnable() {
             @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if (task.isSuccessful()) {
-                    for (QueryDocumentSnapshot categoryDocument : task.getResult()) {
-                        ArrayList<MenuItem> itemsArray = new ArrayList<MenuItem>();
-                        db.collection("menu")
-                                .document(categoryDocument.getId())
-                                .collection("items").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                            @Override
-                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                if (task.isSuccessful()) {
-                                    for (QueryDocumentSnapshot itemDocument : task.getResult()) {
-                                        MenuItem menuItem = new MenuItem(itemDocument.getId(),itemDocument.getString("name"),
-                                                itemDocument.getString("image"),itemDocument.getLong("price"),itemDocument.getString("description"));
-                                        itemsArray.add(menuItem);
-
-                                    }
-                                    Category category = new Category(categoryDocument.getId(),
-                                            categoryDocument.getString("name"),categoryDocument.getString("image"),itemsArray);
-                                    menuArray.add(category);
-                                    categoryAdapter.notifyDataSetChanged();
-                                    menuAdapter.notifyDataSetChanged();
-                                }
-                                else {
-                                    System.out.println("Error getting documents."+ task.getException());
-                                }
-                            }
-                        });
-                    }
-                }
+            public void run() {
+                displayMenu(1);
             }
-        });
-
+        }).start();
     }
+    public void displayMenu(int i) {
+            DocumentReference categoryDocument = db.collection("menu").document("category-"+i);
+            categoryDocument.get()
+                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if (task.isSuccessful()) {
+                                DocumentSnapshot document = task.getResult();
+                                ArrayList<MenuItem> itemsArray = new ArrayList<MenuItem>();
+                                categoryDocument.collection("items").get()
+                                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                                for (QueryDocumentSnapshot itemDocument : task.getResult()) {
+                                                    MenuItem menuItem = new MenuItem(itemDocument.getId(), itemDocument.getString("name"),
+                                                            itemDocument.getString("image"), itemDocument.getLong("price"), itemDocument.getString("description"));
+                                                    itemsArray.add(menuItem);
+                                                }
+                                                if (document.exists()) {
+                                                    Category category = new Category(document.getId(),
+                                                            document.getString("name"),document.getString("image"),itemsArray);
+                                                    menuArray.add(category);
+                                                    categoryAdapter.notifyDataSetChanged();
+                                                    menuAdapter.notifyDataSetChanged();
+                                                    if (i == 1) displayMenu(2);
+                                                    else if (i == 2) displayMenu(3);
+                                                    else if (i == 3) displayMenu(4);
+                                                    else if (i == 4) displayMenu(5);
+                                                    else if (i == 5) displayMenu(6);
+                                                    else if (i == 6) displayMenu(7);
+                                                    else if (i == 7) displayMenu(8);
+                                                }
+                                            }
+                                        });
+
+                                }
+                            else {
+                                System.out.println("Error getting documents."+ task.getException());
+                            }
+                        }
+                    });
+    }
+
+//    public void getMenuArray() {
+//        db.collection("menu")
+//                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                if (task.isSuccessful()) {
+//                    for (QueryDocumentSnapshot categoryDocument : task.getResult()) {
+//                        ArrayList<MenuItem> itemsArray = new ArrayList<MenuItem>();
+//                        db.collection("menu")
+//                                .document(categoryDocument.getId())
+//                                .collection("items")
+//                                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                            @Override
+//                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                                if (task.isSuccessful()) {
+//                                    for (QueryDocumentSnapshot itemDocument : task.getResult()) {
+//                                        MenuItem menuItem = new MenuItem(itemDocument.getId(),itemDocument.getString("name"),
+//                                                itemDocument.getString("image"),itemDocument.getLong("price"),itemDocument.getString("description"));
+//                                        itemsArray.add(menuItem);
+//
+//                                    }
+//                                    Category category = new Category(categoryDocument.getId(),
+//                                            categoryDocument.getString("name"),categoryDocument.getString("image"),itemsArray);
+//                                    menuArray.add(category);
+//                                    categoryAdapter.notifyDataSetChanged();
+//                                    menuAdapter.notifyDataSetChanged();
+//                                }
+//                                else {
+//                                    System.out.println("Error getting documents."+ task.getException());
+//                                }
+//                            }
+//                        });
+//                    }
+//                }
+//            }
+//        });
+//
+//    }
 }

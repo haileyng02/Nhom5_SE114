74bc88c1427c3e98d2f15e03d1277f5969034596
Trang <thutrangpham1509@gmail.com>
2022-05-22 23:18:05 +0700

    set menu toolbar

--- app/src/main/java/com/example/koffi/MenuFragment.java
+++ app/src/main/java/com/example/koffi/MenuFragment.java
@@ -7,7 +7,9 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -78,6 +80,10 @@ public class MenuFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        //Toolbar
+        Toolbar toolbar = view.findViewById(R.id.menu_toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
 
         db = FirebaseFirestore.getInstance();
 

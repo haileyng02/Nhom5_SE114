39fd68d7421cd212fe70ac754797edca5a9be2b6
Khanh20521455 <20521455@gm.uit.edu.vn>
2022-05-28 19:17:46 +0700

    profile Fragment Logic

---
+++ app/src/main/java/com/example/koffi/User.java
@@ -0,0 +1,18 @@
+package com.example.koffi;
+
+public class User {
+    public String Ten, Email, Sdt, NgaySinh, GioiTinh;
+    public User()
+    {
+
+    }
+    public User (String ten, String email,String sdt,String ngaysinh, String gioitinh)
+    {
+
+        this.Ten=ten;
+        this.Email=email;
+        this.Sdt=sdt;
+        this.NgaySinh=ngaysinh;
+        this.GioiTinh=gioitinh;
+    }
+}

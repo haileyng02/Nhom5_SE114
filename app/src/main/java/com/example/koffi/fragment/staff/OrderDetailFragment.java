621f052571286a0ac6bc7f4bc5f02e94c07e3e05
haileyng02 <haileynguyenss@gmail.com>
2022-06-07 10:26:38 +0700

    staff

--- app/src/main/java/com/example/koffi/fragment/staff/OrderDetailFragment.java
+++ app/src/main/java/com/example/koffi/fragment/staff/OrderDetailFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -72,6 +73,15 @@ public class OrderDetailFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        //Toolbar
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.orderdetail_toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().onBackPressed();
+            }
+        });
 
         //Init
         Button cancelBtn = view.findViewById(R.id.orderdetail_cancelBtn);

fdc646e579c3ad50a171c9686c1f467208029ea0
Trang <thutrangpham1509@gmail.com>
2022-06-13 23:56:51 +0700

    view item in order detail and fix lots of bugs

--- app/src/main/java/com/example/koffi/fragment/other/OrderHistoryFragment.java
+++ app/src/main/java/com/example/koffi/fragment/other/OrderHistoryFragment.java
@@ -68,7 +68,7 @@ public class OrderHistoryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        setHasOptionsMenu(true);
+        setHasOptionsMenu(false);
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_order_history, container, false);
     }
--- app/src/main/java/com/example/koffi/fragment/staff/OrderDetailFragment.java
+++ app/src/main/java/com/example/koffi/fragment/staff/OrderDetailFragment.java
@@ -4,12 +4,19 @@ import static android.content.ContentValues.TAG;
 
 import static com.example.koffi.FunctionClass.setListViewHeight;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.res.ColorStateList;
 import android.database.DataSetObserver;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.navigation.Navigation;
@@ -18,6 +25,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -25,10 +33,12 @@ import android.widget.Toast;
 import android.widget.TextView;
 
 import com.example.koffi.R;
+import com.example.koffi.activity.MainActivity;
 import com.example.koffi.adapter.CartItemAdapter;
 import com.example.koffi.dialog.CancelOrderDialog;
 import com.example.koffi.dialog.DialogLogOut;
 import com.example.koffi.models.CartItem;
+import com.example.koffi.models.Topping;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentChange;
@@ -108,6 +118,7 @@ public class OrderDetailFragment extends Fragment {
         TextView note=view.findViewById(R.id.note_order);
         TextView kieunhan=view.findViewById(R.id.kieunhan_txt);
         TextView tvAddress = view.findViewById(R.id.order_tvAddress);
+        TextView tvNote = view.findViewById(R.id.detail_note);
 
         if(getArguments().getString("documentID")!=null)
             orderID=getArguments().getString("documentID");
@@ -142,6 +153,35 @@ public class OrderDetailFragment extends Fragment {
                 }
             }
         });
+        cartList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String text = "Sản phẩm: " + cart.get(i).item;
+                text += "\nSize: " + cart.get(i).size + "\nToppings: ";
+                for (Topping topping : cart.get(i).toppings) {
+                    text += "\n+ " + topping.name;
+                }
+                text += "\nGhi chú: ";
+                if (cart.get(i).note != null && !cart.get(i).note.isEmpty()) {
+                    text += cart.get(i).note;
+                } else text += "Không có ghi chú";
+                AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+                Context context = getContext();
+                alert.setTitle("Thông tin chi tiết: ");
+                alert.setMessage(text);
+                alert.setPositiveButton("Đóng", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                AlertDialog dialog = alert.create();
+                dialog.show();
+                dialog.getButton(dialog.BUTTON_POSITIVE)
+                    .setTextColor(Color.parseColor("#795C34"));
+            }
+        });
+
         //setInfomation
         if(getArguments().getString("documentID")!=null) {
             title = view.findViewById(R.id.order_title);
@@ -186,21 +226,27 @@ public class OrderDetailFragment extends Fragment {
                                     if(document.getString("deliveryNote")!=null)
                                         if(!document.getString("deliveryNote").equals(""))
                                             note.setText(document.getString("deliveryNote"));
-                                    if (document.getLong("status") == 1)
+                                        long status = document.getLong("status");
+                                    if (status == 1)
                                         state.setText("Chờ xác nhận");
-                                    else if (document.getLong("status") == 2)
+                                    else if (status == 2)
                                         state.setText("Đang chuẩn bị");
-                                    else if (document.getLong("status") == 3)
+                                    else if (status == 3)
                                         state.setText("Đang giao hàng");
-                                    else if (document.getLong("status") == 4)
+                                    else if (status == 4)
                                         state.setText("Đã hoàn thành");
-                                    if (document.getLong("status") == 1)
+                                    if (status == 1)
                                         btnchangeState.setText("Xác nhận đơn hàng");
-                                    else if (document.getLong("status") == 2)
+                                    else if (status == 2)
                                         btnchangeState.setText("Xác nhận chuẩn bị xong");
-                                    else if (document.getLong("status") == 3)
+                                    else if (status == 3)
                                         btnchangeState.setText("Xác nhận đã giao");
-
+                                    if (status == 5) {
+                                        btnchangeState.setVisibility(View.GONE);
+                                        cancelBtn.setVisibility(View.GONE);
+                                        state.setText("Đã hủy");
+                                        tvNote.setText("Lý do hủy: ");
+                                    }
                                     btnchangeState.setOnClickListener(new View.OnClickListener() {
                                         @Override
                                         public void onClick(View view) {

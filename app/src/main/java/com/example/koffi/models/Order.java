08842ec64d843cf19af489dcce1144356d3f0374
haileyng02 <haileynguyenss@gmail.com>
2022-06-13 20:43:45 +0700

    Home new look

--- app/src/main/java/com/example/koffi/models/Order.java
+++ app/src/main/java/com/example/koffi/models/Order.java
@@ -58,11 +58,6 @@ public class Order implements Comparable<Order>{
         this.status = status;
     }
 
-    public Date confirmTime;
-    public Date serveTime;
-    public Date deliTime;
-    public Date cancelTime;
-
     @Override
     public int compareTo(Order order) {
         return this.date.compareTo(order.date);
